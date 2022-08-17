0125f9c4e264c15d0d116ec54824442aa5e069aa
mosqu1t0 <mosquito@email.cn>
2022-08-17 20:15:06 +0800

    logo check

--- src/components/utils/printLogo.js
+++ src/components/utils/printLogo.js
@@ -1,25 +1,16 @@
 
-const makeMulti = function (logo) {
-    let l = new String(logo)
-    l = l.substring(l.indexOf("/*") + 3, l.lastIndexOf("*/"))
-    return l
-}
-const logo = () => {
-/*
 
+const logo = 
+`
  ██████╗ ██████╗ ██████╗ ███████╗███████╗██╗  ██╗███████╗███████╗██████╗ 
 ██╔════╝██╔═══██╗██╔══██╗██╔════╝██╔════╝██║  ██║██╔════╝██╔════╝██╔══██╗
 ██║     ██║   ██║██║  ██║█████╗  ███████╗███████║█████╗  █████╗  ██████╔╝
 ██║     ██║   ██║██║  ██║██╔══╝  ╚════██║██╔══██║██╔══╝  ██╔══╝  ██╔═══╝ 
 ╚██████╗╚██████╔╝██████╔╝███████╗███████║██║  ██║███████╗███████╗██║     
  ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═╝╚══════╝╚══════╝╚═╝     
-                                                                         
-*/
-    console.log("what")
-}
+`;                                                                         
 
 export function printLOGO(){
-    let str = makeMulti(logo);
-    console.log(`%c${str} %cV1.0 Power by fun`, 'color: #2da44e', 'color: #2da44e;font-size: 0.75rem;font-style: italic;');
+    console.log(`%c${logo} %cV1.0 Power by fun`, 'color: #2da44e', 'color: #2da44e;font-size: 0.75rem;font-style: italic;');
     console.log('%c', 'color: #2da44e;font-size: 1rem')
 }
