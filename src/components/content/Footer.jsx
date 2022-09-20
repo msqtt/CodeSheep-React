414ecc1a3b1a47cc35f5bbf5904e9fdd3f700ad6
mosqu1t0 <mosquito@email.cn>
2022-09-20 13:10:25 +0800

    peian

--- src/components/content/Footer.jsx
+++ src/components/content/Footer.jsx
@@ -1,9 +1,27 @@
-import * as React from 'react'
+import * as React from "react";
+import img from "../../assets/110.png";
 
 function Footer() {
-    return ( 
-        <footer>No Licensed | Copyright © 2022-present <a className='perfectA' href='https://github.com/mosqu1t0'>mosquito</a></footer>
-    );
+  return (
+    <footer>
+      {" "}
+      <a
+        href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=45032302000093"
+        style={{
+          display: "inline-block",
+          textDecoration: "none",
+          color: "#4e6e8e",
+        }}
+      >
+        <img alt="" src={img} style={{ float: "left" }} />
+        &nbsp;桂公网安备 45032302000093号
+      </a>{" "}
+      | Copyright © 2022-present{" "}
+      <a className="perfectA" href="https://github.com/mosqu1t0">
+        mosquito
+      </a>
+    </footer>
+  );
 }
 
 export default Footer;
