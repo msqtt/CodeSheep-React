e8f5e3450417c912ee54435d7d4a219e4c840f15
mosqu1t0 <mosquito@email.cn>
2022-09-20 13:37:58 +0800

    change size bg

--- src/components/utils/yepyep.js
+++ src/components/utils/yepyep.js
@@ -1,9 +1,8 @@
-
-export function yep(){
-    let yep = localStorage.getItem('yep');
-    if (yep === 'yep') {
-        document.body.style.backgroundImage = 'url("https://raw.githubusercontent.com/mosqu1t0/Sources/master/Img/yep.jpg")';
-        document.body.style.backgroundSize = '24%';
-    }
+export function yep() {
+  let yep = localStorage.getItem("yep");
+  if (yep === "yep") {
+    document.body.style.backgroundImage =
+      'url("https://raw.githubusercontent.com/mosqu1t0/Sources/master/Img/yep.jpg")';
+    document.body.style.backgroundSize = "18%";
+  }
 }
-
