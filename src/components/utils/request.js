eb8fc07714b970b6c3f0e2631c2236844e6f9280
mosqu1t0 <mosquito@email.cn>
2022-10-08 22:40:45 +0800

    request

--- src/components/utils/request.js
+++ src/components/utils/request.js
@@ -1,50 +1,53 @@
 import axios from "axios";
 
 export function GET(url, params, headers) {
-    return new Promise((resolve, reject) => {
-        axios.get(url, {...params}, {...headers})
-            .then(response => {
-                resolve(response.data);
-            })
-            .catch(error => {
-                reject(error.data);
-            })
-    })
+  return new Promise((resolve, reject) => {
+    axios
+      .get(url, { ...params }, { ...headers })
+      .then((response) => {
+        resolve(response.data);
+      })
+      .catch((error) => {
+        reject(error.data);
+      });
+  });
 }
 
 export function POST(url, params, headers) {
-    return new Promise((resolve, reject) => {
-        axios.post(url, {...params}, {...headers})
-            .then(response => {
-                resolve(response.data);
-            })
-            .catch(error => {
-                reject(error.data);
-            })
-    })
+  return new Promise((resolve, reject) => {
+    axios
+      .post(url, { ...params }, { ...headers })
+      .then((response) => {
+        resolve(response.data);
+      })
+      .catch((error) => {
+        reject(error.data);
+      });
+  });
 }
 
 export function DELETE(url, params, headers) {
-    return new Promise((resolve, reject) => {
-        axios.delete(url, {...params}, {...headers})
-            .then(response => {
-                resolve(response.data);
-            })
-            .catch(error => {
-                reject(error.data);
-            })
-    })
+  return new Promise((resolve, reject) => {
+    axios
+      .delete(url, { ...params }, { ...headers })
+      .then((response) => {
+        resolve(response.data);
+      })
+      .catch((error) => {
+        reject(error.data);
+      });
+  });
 }
 
-
 export function PUT(url, params, headers) {
-    return new Promise((resolve, reject) => {
-        axios.put(url, {...params}, {...headers})
-            .then(response => {
-                resolve(response.data);
-            })
-            .catch(error => {
-                reject(error.data);
-            })
-    })
+  return new Promise((resolve, reject) => {
+    axios
+      .put(url, { ...params }, { ...headers })
+      .then((response) => {
+        resolve(response.data);
+      })
+      .catch((error) => {
+        reject(error.data);
+      });
+  });
 }
