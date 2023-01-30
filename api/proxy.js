fdf7d2712328abadf3060d74b6e988ae8623ca36
mosquito <mosquito@email.cn>
2023-01-30 19:50:48 +0800

    add backend api

--- api/proxy.js
+++ api/proxy.js
@@ -7,7 +7,7 @@ module.exports = (req, res) => {
   // 代理目标地址
   // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
   if (req.url.startsWith("/api")) {
-    target = "localhost";
+    target = "123.60.176.7:8080";
   }
   // 创建代理对象并转发请求
   createProxyMiddleware({
