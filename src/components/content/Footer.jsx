ecfa402178c141f42840267b1d2e044070b1d23a
mosquito <mosquito@email.cn>
2023-01-30 19:22:22 +0800

    rm foot domain shit info

--- src/components/content/Footer.jsx
+++ src/components/content/Footer.jsx
@@ -5,19 +5,7 @@ function Footer() {
   return (
     <footer>
       {" "}
-      <a
-        href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=45032302000093"
-        target="_blank"
-        style={{
-          display: "inline-block",
-          textDecoration: "none",
-          color: "#4e6e8e",
-        }}
-      >
-        <img alt="" src={img} style={{ float: "left" }} />
-        &nbsp;桂公网安备 45032302000093号
-      </a>{" "}
-      | Copyright © 2022-present{" "}
+      Copyright © 2022-present{" "}
       <a className="perfectA" href="https://github.com/mosqu1t0">
         mosquito
       </a>
